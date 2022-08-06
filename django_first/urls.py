73bd771df7454bb0b365a0627e2b54297d879550
PureWater777 <peter9797@wp.pl>
2022-08-06 14:51:11 +0200

    admin models registration

--- django_first/urls.py
+++ django_first/urls.py
@@ -17,6 +17,8 @@ import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'Ebiedronka'
+admin.site.index_title = 'Admin'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
