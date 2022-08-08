1e21afbb52ad7fc61ef60e295cf26262969f3705
PureWater777 <peter9797@wp.pl>
2022-08-08 21:15:25 +0200

    add Collection serializers and endpoints

--- store/serializers.py
+++ store/serializers.py
@@ -1,7 +1,33 @@
+from decimal import Decimal
+
+from django.db.models import Count
 from rest_framework import serializers
+from store.models import Product, Collection
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+# Model serializers
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title', 'products_count']
+    products_count = serializers.IntegerField()
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
+
+
+    #for collection hyperlinks
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset=Collection.objects.all(),
+    #     view_name='collection-detail'
+    # )
+    #for nested objects representation
+    #collection = ProductSerializer()
+
